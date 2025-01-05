14cf9dee9e92b9ff48af4f0a3e1f5bd1684956ac
Yusuf Msabeh <yussef.msabeh@gmail.com>
2025-01-05 10:55:00 +0200

    feat: configure server to use port from .env file

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const connection = require("./database/config");
+
+const PORT = process.env.SERVER_PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 connection.authenticate().then(() => {
   connection.sync({ alter: true }).then(() => {
-    app.listen(3000, "localhost", () =>
-      console.log(" Server started on port 3000"),
+    app.listen(PORT, "localhost", () =>
+      console.log(" Server started on port ", PORT),
     );
   });
 });
