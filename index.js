e0f0add2692bba96789096c663f8913ecb59adcd
Yusuf Msabeh <yussef.msabeh@gmail.com>
2025-01-23 10:16:07 +0200

    refactor: make the the entry point (index.js) more readable

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-// loading the environment variables
-const dotenv = require("dotenv");
-dotenv.config();
-
 const express = require("express");
+const configureExpress = require("./services/configureExpress");
 const connection = require("./database/config");
-const authRouter = require("./routes/auth");
-const chatRouter = require("./routes/message");
-const groupRouter = require("./routes/group");
-const multer = require("multer");
-const upload = multer();
-
-const PORT = process.env.SERVER_PORT || 5000;
+const routes = require("./routes/index");
 
 const app = express();
-app.use(upload.any());
+// configure the webserver
+configureExpress(app);
+// assigning routes
+routes(app);
 
-// public endpoints
-app.use("/auth", authRouter);
-app.use("/chatting", chatRouter);
-app.use("/group", groupRouter);
+const PORT = process.env.SERVER_PORT || 5000;
+const HOST = process.env.HOST;
+// checking and syncing database connection and start the server
 connection.authenticate().then(() => {
   connection.sync().then(() => {
-    app.listen(PORT, "localhost", () =>
-      console.log(" Server started on port ", PORT),
-    );
+    app.listen(PORT, HOST, () => console.log(" Server started on port ", PORT));
   });
 });
